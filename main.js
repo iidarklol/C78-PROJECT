b0060da117abf7194840e16956aad37c2e93580a
iidarklol <77841200+iidarklol@users.noreply.github.com>
2021-04-28 13:47:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name="Very"
+console.log(first_name)
+var last_name="Cool"
+console.log(last_name);
+var full_name= first_name.concat(last_name)
+console.log(full_name);
+var a = 100000;
+console.log(a)
+var b = 500000;
+console.log(b)
+var add = a + b 
+console.log(add)
